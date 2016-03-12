ee42926e20335bf4fd7c2b682aff52a85f63efaa
sigepon <info@sigepon-jp.com>
2016-03-13 00:10:57 +0900

    設定ファイルを追加

--- config.php
+++ config.php
@@ -1,3 +1,9 @@
 <?php
 
-echo 'hello';
+// DB接続に必要な情報
+define('DSN', 'mysql:host=localhost;dbname=login_system;charset=utf8');
+define('USER', 'testuser');
+define('PASSWORD', '9999');
+
+// エラーレベルの設定
+error_reporting(E_ALL & ~E_NOTICE);
